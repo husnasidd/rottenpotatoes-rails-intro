f6b41d5c6e6c7ab284cfa975c4395190366a4c31
husnasidd <hsiddiqu@citadel.edu>
2015-10-09 01:26:25 +0000

    rails hw part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,22 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    get_all_ratings()
+    get_checked_boxes()
+    
+    if(params[:ratings])
+      #:ratings is a hash of the rating which is passed in
+      @ratingshash = params[:ratings]
+      #justKeys should be something like [G,PG13]
+      #if you call keys on a hash it will return just keys 
+      @justKeys = @ratingshash.keys
+      @movies = Movie.where(rating: @justKeys)
+    
+    else
+      @movies = Movie.all
+      @justKeys = @all_ratings
+    end
+
     #if sort by was set to "title" then order by the titles
       if(params[:sort_by] == "title")
         @movies = Movie.order(:title)
@@ -24,7 +40,6 @@ class MoviesController < ApplicationController
       if(params[:sort_by] == "release_date")
         @movies = Movie.order(:release_date)
         @releasedateclass = "hilite"
-        
       end
   end
 
@@ -56,6 +71,24 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def get_all_ratings
+    # pluck selects one column from a table
+    # uniq selects only unique values -- won't repeat the ratings 
+    @all_ratings = Movie.uniq.pluck(:rating)
+  end
+  
+  def get_checked_boxes
+    @check_box_hash = Hash.new
+    @check_box_hash = {"PG" => true ,"PG13" => true , "G" => true, "R" => true}
+    # @ratingshash = params[:ratings]
+    # @check_box_hash = {@ratingshash}
+    # for element in @check_box_hash
+    #   if(element.value == 1)
+    #     then element.value = true
+      
+  
+  end
+  
   private :movie_params
   
 end

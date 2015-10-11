b75da379eed556ed3d0219e165a25c2bab572694
husnasidd <hsiddiqu@citadel.edu>
2015-10-11 22:58:35 +0000

    finished part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,93 +16,12 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     get_all_ratings()
     
-    # # check if params[:ratings] and paramas[:sort_by] is null and the session[:ratings] 
-    # # and session[:sort_by] is not null
-    # # this is the only case when we should update the params to match the saved session
-    # if(params[:sort_by].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?))
-    #   redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) and return 
-    # # in any other case session should be updated to match params
-    # end
-    
-    # if(params[:sort_by].nil? && params[:ratings].nil? && (session[:sort].nil? && session[:ratings].nil?))
-    #   @movies = Movie.all
-    # end
-    
-    # @ratings = params[:ratings]
-    # @sort = params[:sort_by]
-    # if(@ratings.nil?)
-    #   @justKeys = @all_ratings
-    # else
-    #   @justKeys = params[:ratings].keys
-    # end
-    
-    
-    # if(!@sort.nil?)
-    #   if(!@ratings.nil?)
-    #     @movies = Movie.where(rating: @justKeys).order(session[:sort_by])
-    #   else
-    #     @movies = Movie.order(session[:sort_by])
-    #   end
-    # else
-    #   @movies = Movie.all
-    # end
-     
-    
-    # session[:ratings] = @ratings
-    # session[:sort_by] = @sort
-      
-    
-    # if(params[:ratings].nil? && session[:ratings].nil? && params[:sort_by].nil? && session[:sort_by].nil? || {})
-    #     @justKeys = @all_ratings
-    #     @movies = Movie.all
-    # end
-    
-    # if(params[:ratings].nil? && params[:sort_by].nil? && 
-    #   (!session[:sort_by].nil? && !session[:ratings].nil?))
-    #     params[:ratings] = session[:ratings]
-    #     params[:sort_by] = session[:sort_by]
-    #     @justKeys = params[:ratings].keys()
-    #     redirect_to movies_path(params) and return
-    # end
-    
-    # if(!params[:sort_by].nil?) #sort by is not null
-    #   if(!params[:ratings].nil?) # ratings is not null 
-    #     session[:ratings] = params[:ratings]
-    #     session[:sort_by] = params[:sort_by]
-    #     @justKeys = session[:ratings].keys
-    #     @movies = Movie.where(rating: @justKeys).order(params[:sort_by])
-    #   else #ratings is null 
-    #     session[:sort_by] = params[:sort_by]
-    #     @justKeys = @all_ratings
-    #     @movie = Movie.order(params[:sort_by])
-    #   end
-    # else #sort by is null
-    #   @movies = Movie.where(rating: @justKeys)
-    # end
-    
-    # if(!params[:ratings].nil?)
-    #   if(!params[:sort_by].nil?)
-    #     session[:ratings] = params[:ratings]
-    #     session[:sort_by] = params[:sort_by]
-    #     @justKeys = session[:ratings].keys
-    #     @movies = Movie.where(rating: @justKeys).order(params[:sort_by])
-    #   else
-    #     session[:ratings] = params[:ratings]
-    #     @justKeys = params[:ratings].keys
-    #     @movies = Movie.where(rating: @justKeys)
-    #   end
-    # else
-    #   @movies = Movie.order(params[:sort_by])
-    # end
-    
-      
-        
-        
-      # check if params[:ratings] and paramas[:sort_by] is null and the session[:ratings] 
+    # check if params[:ratings] and paramas[:sort_by] is null and the session[:ratings] 
     # and session[:sort_by] is not null
     # this is the only case when we should update the params to match the saved session
     if(params[:sort_by].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?))
       @justKeys = session[:ratings].keys()
+      flash.keep(:notice)
       redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) and return 
     # in any other case session should be updated to match params
     end
@@ -138,7 +57,12 @@ class MoviesController < ApplicationController
           @movies = Movie.where(rating: @justKeys).order(session[:sort_by])
         end
       end
-      
+      if(params[:sort_by] == "title")
+        @titleclass = "hilite"
+      end
+      if(params[:sort_by] == "release_date")
+        @releasedateclass = "hilite"
+      end
     end
 
   end
@@ -150,6 +74,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
+    # flash.keep
     redirect_to movies_path
   end
 
@@ -161,6 +86,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
+    # flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -168,6 +94,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
+    # flash.keep
     redirect_to movies_path
   end
 

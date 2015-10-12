cbe1129cd2d5cce98ff956fa1075ae808cbd0d69
husnasidd <hsiddiqu@citadel.edu>
2015-10-12 00:25:42 +0000

    added flash.keep

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -78,7 +78,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
-    # flash.keep
+    flash.keep
     redirect_to movies_path
   end
 
@@ -90,7 +90,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
-    # flash.keep
+    flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -98,7 +98,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    # flash.keep
+    flash.keep
     redirect_to movies_path
   end
 
